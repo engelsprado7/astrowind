c9aff1a11e60b391e80f651867fcc2ff2259b935
Engels <engelsprado7@gmail.com>
2024-06-06 11:32:27 -0600

    logo footer updated

--- src/navigation.js
+++ src/navigation.js
@@ -182,7 +182,6 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="src/assets/images/logo.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> Segundo Sueldo</a> · All rights reserved.
+
   `,
 };
